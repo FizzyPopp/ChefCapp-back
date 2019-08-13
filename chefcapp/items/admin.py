cb867bb9f04ee10939117a29a4378bafe8d63558
FizzyPopp <ardakizilcay@hotmail.com>
2019-08-13 11:50:03 -0400

    Recipe model added to admin interface

--- chefcapp/items/admin.py
+++ chefcapp/items/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+
 from .models import Ingredient
 
 admin.site.register(Ingredient)
+
+from .models import Recipe
+
+admin.site.register(Recipe)
 
 # Register your models here.
