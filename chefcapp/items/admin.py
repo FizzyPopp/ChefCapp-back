f4cafa1a89f0e5b1f28712539d666c6330265657
FizzyPopp <ardakizilcay@hotmail.com>
2019-08-13 17:02:34 -0400

    horizontal field attempt

--- chefcapp/items/admin.py
+++ chefcapp/items/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Ingredient
 from .models import Recipe
 
+class YourAdmin(admin.ModelAdmin):
+    filter_horizontal = ('m2m_field',)
+
+admin.site.register(YourAdmin)
 admin.site.register(Ingredient)
 admin.site.register(Recipe)
 
