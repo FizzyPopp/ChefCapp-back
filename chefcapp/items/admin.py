d35e2ac5edd602cd96f33f466c5767347ba3ba60
FizzyPopp <ardakizilcay@hotmail.com>
2019-08-13 15:34:06 -0400

    Trying to add units and steps

--- chefcapp/items/admin.py
+++ chefcapp/items/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-
 from .models import Ingredient
-
-admin.site.register(Ingredient)
-
 from .models import Recipe
 
+admin.site.register(Ingredient)
 admin.site.register(Recipe)
 
 # Register your models here.
--- chefcapp/items/models.py
+++ chefcapp/items/models.py
@@ -3,9 +3,42 @@ from django.db import models
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=256)
-    
+
     class Meta:
         db_table = 'ingredients'
+
+
+class Unit(models.Model):
+    milliliter = 'ml'
+    grams = 'gr'
+    unspecified = ''
+
+    unit_choices = [
+        (milliliter, 'Milliliters'),
+        (grams, 'Grams'),
+        (unspecified, 'Unspecified')
+    ]
+
+    units = models.CharField(
+        max_length = 2,
+        choices = unit_choices,
+        default = unspecified,
+    )
+
+    class Meta:
+        db_table = 'units'
+
+
+class UnitIngredient(models.Model):
+    unit = models.ForeignKey(
+        Unit,
+        on_delete=models.CASCADE,
+    )
+
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+    )
 
 
 class Equipment(models.Model):
@@ -16,11 +49,15 @@ class Equipment(models.Model):
         db_table = 'equipment'
 
 
+class Step(models.Model):
+    number = models.PositiveSmallIntegerField()
+    description = models.CharField(max_length=256)
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=256)
     ingredients = models.ManyToManyField(Ingredient)
-    required_equipment = models.ManyToManyField(Equipment)
-    description = models.TextField()
+    steps = models.ManyToManyField(Step)
 
     class Meta:
         db_table = 'recipes'
