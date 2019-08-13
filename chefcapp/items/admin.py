5b248edb4807b491fe8d827b381589a0fa583287
FizzyPopp <ardakizilcay@hotmail.com>
2019-08-13 17:41:00 -0400

    horizontal field fourth attempt

--- chefcapp/items/admin.py
+++ chefcapp/items/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Ingredient
 from .models import Recipe
 
-class IngredentAdmin(admin.ModelAdmin):
+class IngredientAdmin(admin.ModelAdmin):
     filter_horizontal = (Ingredient,)
 
 
