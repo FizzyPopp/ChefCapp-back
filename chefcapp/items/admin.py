624b2eec8a7e8f2a969d2dc01adc0db8e4dca700
FizzyPopp <ardakizilcay@hotmail.com>
2019-08-13 17:20:00 -0400

    horizontal field second attempt

--- chefcapp/items/admin.py
+++ chefcapp/items/admin.py
@@ -3,7 +3,8 @@ from .models import Ingredient
 from .models import Recipe
 
 class YourAdmin(admin.ModelAdmin):
-    filter_horizontal = ('m2m_field',)
+    filter_horizontal = (Ingredient,)
+
 
 admin.site.register(YourAdmin)
 admin.site.register(Ingredient)
