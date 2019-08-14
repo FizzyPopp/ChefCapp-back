cd52a5e887b4af5c990f0dc36613db84e321af90
FizzyPopp <ardakizilcay@hotmail.com>
2019-08-14 15:38:59 -0400

    more changes to models and admin

--- chefcapp/items/admin.py
+++ chefcapp/items/admin.py
@@ -4,12 +4,16 @@ from .models import Recipe
 from .models import Step
 from .models import UnitIngredient
 
-class YourAdmin(admin.ModelAdmin):
-    filter_horizontal = ('ingredients','steps','ingredients_for_step')
+class HorizontalRecipe(admin.ModelAdmin):
+    filter_horizontal = ('ingredients','steps')
 
 
-admin.site.register(Recipe, YourAdmin)
-admin.site.register(Step)
+class HorizontalStep(admin.ModelAdmin):
+    filter_horizontal = ('ingredients_for_step')
+
+
+admin.site.register(Recipe, HorizontalRecipe)
+admin.site.register(Step, HorizontalStep)
 admin.site.register(Ingredient)
 admin.site.register(UnitIngredient)
 
