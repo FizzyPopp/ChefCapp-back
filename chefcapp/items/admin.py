e808a01ea08dc498ed7587d020092c4a45250c1d
FizzyPopp <ardakizilcay@hotmail.com>
2019-08-14 15:26:36 -0400

    adding steps to admin

--- chefcapp/items/admin.py
+++ chefcapp/items/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from .models import Ingredient
 from .models import Recipe
+from .models import Step
 
 class YourAdmin(admin.ModelAdmin):
     filter_horizontal = ('ingredients',)
 
 
 admin.site.register(Recipe, YourAdmin)
+admin.site.register(Step)
 admin.site.register(Ingredient)
 
 # Register your models here.
